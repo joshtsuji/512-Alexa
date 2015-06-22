d39910d43e346e632ebbc80d85f1397161d5851a
Joshua Tsuji <jtsuji@hubspot.com>
2015-06-22 02:13:01 -0400

    http

--- src/main/java/hue/LightingClient.java
+++ src/main/java/hue/LightingClient.java
@@ -5,4 +5,9 @@ package hue;
  */
 public class LightingClient {
 
+    public static final String SUNSET_BED = "c30603940-on-0";
+    public static final String TURQUIOSE_BED = "0e397dcd9-on-0";
+
+
+
 }
