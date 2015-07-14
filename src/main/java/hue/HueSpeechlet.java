a7155b9983abb5f6564e3241cb38e108b429f443
Joshua Tsuji <jtsuji@hubspot.com>
2015-07-14 00:14:31 -0400

    no debug prompt

--- src/main/java/hue/HueSpeechlet.java
+++ src/main/java/hue/HueSpeechlet.java
@@ -67,7 +67,7 @@ public class HueSpeechlet extends HttpServlet implements Speechlet {
         String scene = LightingClient.changeLights(reqScene);
 
         return buildSpeechletResponse("Lighting",
-                "Okay, changing lights to " + scene + ". I heard you ask for " + reqScene,
+                "Okay, changing lights to " + scene + ".",
                 "", true);
     }
 
