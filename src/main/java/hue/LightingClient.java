ee6c3a95d19678ce9e9b0b035b31e1e1c5b7c6fb
Joshua Tsuji <jtsuji@hubspot.com>
2015-07-24 00:40:40 -0400

    more scenes

--- src/main/java/hue/LightingClient.java
+++ src/main/java/hue/LightingClient.java
@@ -20,7 +20,10 @@ public class LightingClient {
 
     static {
         nameToId.put("light green", "afd39e8fa-on-0");
-        nameToId.put("green", "4d0acae0f-on-0");
+        nameToId.put("green", "6f4eec56f-on-0");
+        nameToId.put("red", "cc0afa4b2-on-0");
+        nameToId.put("pink", "97d596458-on-0");
+        nameToId.put("yellow", "c141feb9f-on-0");
         nameToId.put("primaries", "b7a556b43-on-0");
         nameToId.put("turquoise", "0dd0eb23b-on-0");
         nameToId.put("rainbow", "a36922ffb-on-0");
