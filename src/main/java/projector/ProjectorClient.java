ac418fe9aee0bb7321ee04b15ef559db8708f1c0
Joshua Tsuji <jtsuji@hubspot.com>
2015-07-24 01:49:26 -0400

    actually do it

--- src/main/java/projector/ProjectorClient.java
+++ src/main/java/projector/ProjectorClient.java
@@ -1,23 +1,34 @@
 package projector;
 
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 /**
  * Created by hubspot on 7/24/15.
  */
 public class ProjectorClient {
     public static void turnOn() {
-        Unirest.get("http://146.115.86.220:85/event?projector");
+        try {
+            Unirest.get("http://146.115.86.220:85/event?projector").asString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     public static void turnOff() {
-        Unirest.get("http://146.115.86.220:85/event?projector");
         try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            Unirest.get("http://146.115.86.220:85/event?projector").asString();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Unirest.get("http://146.115.86.220:85/event?projector").asString();
         }
-        Unirest.get("http://146.115.86.220:85/event?projector");
+        catch (Exception e) {
+
+        }
     }
 }
