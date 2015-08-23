b9eaac2733b5cfbcd8f4e8c402dc2d38f99ba682
Joshua Tsuji <jtsuji@hubspot.com>
2015-08-23 02:33:25 -0400

    more timers

--- src/main/java/event/Mode.java
+++ src/main/java/event/Mode.java
@@ -13,24 +13,40 @@ import java.util.Date;
 public class Mode {
     Date timeActive = new Date();
     long turnOffDelay = 1000 * 10;
+
+    Timer safetyTimer;
+
+
     public void handleBarProximity(boolean active) {
         if (active) {
             timeActive = new Date();
             LightingClient.changeSingleLight("bar", 0, 255, 255, 1000);
+
+            if (safetyTimer != null)
+                safetyTimer.stop();
+
+            safetyTimer = new Timer((int)(new Date().getTime() - timeActive.getTime()), new ActionListener() {
+
+                public void actionPerformed(ActionEvent arg0) {
+                    LightingClient.changeSingleLight("bar", 0, 0, 0, 5000);
+                }
+            });
+            safetyTimer.setRepeats(false);
+            safetyTimer.start();
         }
         else {
             if (new Date().getTime() - timeActive.getTime() < turnOffDelay) {
                 Timer timer = new Timer((int)(new Date().getTime() - timeActive.getTime()), new ActionListener() {
 
                     public void actionPerformed(ActionEvent arg0) {
-                        LightingClient.changeSingleLight("bar", 0, 0, 0, 10000);
+                        LightingClient.changeSingleLight("bar", 0, 0, 0, 5000);
                     }
                 });
                 timer.setRepeats(false);
                 timer.start();
             }
             else {
-                LightingClient.changeSingleLight("bar", 0, 0, 0, 10000);
+                LightingClient.changeSingleLight("bar", 0, 0, 0, 5000);
             }
         }
     }
