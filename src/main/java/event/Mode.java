c5d5eb9834a28717615c8f6db102b61cab1d08bc
Joshua Tsuji <jtsuji@hubspot.com>
2015-08-23 02:23:56 -0400

    Hello timer

--- src/main/java/event/Mode.java
+++ src/main/java/event/Mode.java
@@ -2,6 +2,9 @@ package event;
 
 import hue.LightingClient;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Date;
 
 /**
@@ -9,13 +12,26 @@ import java.util.Date;
  */
 public class Mode {
     Date timeActive = new Date();
+    long turnOffDelay = 1000 * 10;
     public void handleBarProximity(boolean active) {
         if (active) {
             timeActive = new Date();
-            LightingClient.changeSingleLight("bar", 0, 255, 255, 0);
+            LightingClient.changeSingleLight("bar", 0, 255, 255, 1000);
         }
         else {
-            LightingClient.changeSingleLight("bar", 0, 0, 0, 10000);
+            if (new Date().getTime() - timeActive.getTime() < turnOffDelay) {
+                Timer timer = new Timer((int)(new Date().getTime() - timeActive.getTime()), new ActionListener() {
+
+                    public void actionPerformed(ActionEvent arg0) {
+                        LightingClient.changeSingleLight("bar", 0, 0, 0, 10000);
+                    }
+                });
+                timer.setRepeats(false);
+                timer.start();
+            }
+            else {
+                LightingClient.changeSingleLight("bar", 0, 0, 0, 10000);
+            }
         }
     }
 }
