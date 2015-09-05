eda965c92095d8012770618e1c19021fafc94c95
Joshua Tsuji <jtsuji@hubspot.com>
2015-09-05 19:11:29 -0400

    night mode

--- src/main/java/hue/LightingClient.java
+++ src/main/java/hue/LightingClient.java
@@ -38,6 +38,7 @@ public class LightingClient {
         nameToId.put("underwater", "0b58d5a80-on-0");
         nameToId.put("blue", "0b58d5a80-on-0");
         nameToId.put("blue and white", "0b58d5a80-on-0");
+        nameToId.put("night mode", "1e22c6c3d-on-0");
 
         nameToBedroomId.put("sunset", "db4cf55c3-on-0");
         nameToBedroomId.put("turqoise", "33e6af684-on-0");
